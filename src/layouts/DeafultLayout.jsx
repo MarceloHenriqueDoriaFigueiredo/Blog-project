eb3ec87e4b87b4097bd2a841c097831a1d786737
Marcelo Figueiredo <marchenrique99@gmail.com>
2023-03-27 17:29:20 -0300

    Update DeafultLayout.jsx

--- src/layouts/DeafultLayout.jsx
+++ src/layouts/DeafultLayout.jsx
@@ -1,6 +1,5 @@
-import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header/Header";
-
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components/Header/Header';
 
 export function DefaultLayout() {
     return(
